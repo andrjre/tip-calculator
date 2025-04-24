b6916f3a254b1cf9a77b48105345705eb3c8a63a
andrjre <andrjre@gmail.com>
2025-04-24 10:37:53 +0100

    Readability.

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ let submit = document.getElementById("submit")
 let result = document.getElementById("result")
 let tipTotal = document.getElementById("tipTotal")
 
-
-
 submit.onclick = function(){
     result.textContent = "In total you pay " + (billTotal.value * 1.2)
     tipTotal.textContent ="The server recieves " +  (((billTotal.value * 1.2) - billTotal.value))
