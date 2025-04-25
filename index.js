7c476167437920c3e1c8e6cec4ccd2f706d39242
andrjre <andrjre@gmail.com>
2025-04-25 16:54:33 +0100

    Fixed rounding issue.

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ let result = document.getElementById("result")
 let tipTotal = document.getElementById("tipTotal")
 
 submit.onclick = function(){
-    result.textContent = "In total you pay " + (billTotal.value * 1.2)
-    tipTotal.textContent ="The server recieves " +  (((billTotal.value * 1.2) - billTotal.value))
+    
+    
+    result.textContent = "In total you pay " + "£" + Math.round(billTotal.value * 1.2)
+    tipTotal.textContent ="The server recieves " +  "£" + ((Math.round(billTotal.value * 1.2) - billTotal.value))
+    
+    if(billTotal.value < 3){
+        tipTotal.textContent = "dont bother"
+    }
 }
+   
 
 // find out how to set to 2 decimal places
-// implement people divider 
+// implement people divider and tip % option
